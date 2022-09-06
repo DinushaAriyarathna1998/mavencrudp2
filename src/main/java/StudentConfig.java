d20de8f425b15dc24d935cff6f4cf72c76a9e396
dinushaariyarathna <it19181448@my.sliit.lk>
2022-09-06 23:31:06 +0530

    Implemented database property file using spring annotations

--- src/main/java/StudentConfig.java
+++ src/main/java/StudentConfig.java
@@ -1,9 +1,41 @@
+
+import StudentPackage.dao.StudentDBUtil;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @ComponentScan({"StudentPackage"})
-public class StudentConfig {
+@PropertySource("classpath:application.properties")
+public class StudentConfig implements InitializingBean {
 
+
+    @Autowired
+    Environment env;
+
+    @Value( "${db.url}" )
+    private String jdbcUrl;
+
+    @Value( "${db.username}" )
+    private String username;
+
+    @Value( "${db.password}" )
+    private String password;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        setDatabaseConfig();
+    }
+
+    private void setDatabaseConfig() {
+
+        StudentDBUtil.setUrl(jdbcUrl);
+        StudentDBUtil.setUsername(username);
+        StudentDBUtil.setPassword(password);
+    }
 
 }
--- src/main/java/StudentPackage/dao/StudentDBUtil.java
+++ src/main/java/StudentPackage/dao/StudentDBUtil.java
@@ -1,22 +1,44 @@
 package StudentPackage.dao;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+@Component
 public class StudentDBUtil {
 
-    private static String jdbcURL = "jdbc:mysql://localhost:3306/student";
+
+    public static void setUrl(String url) {
+        Url = url;
+    }
+
+    public static void setUsername(String username) {
+        Username = username;
+    }
+
+    public static void setPassword(String password) {
+        Password = password;
+    }
+
+    private static String Url;
+    private static String Username;
+    private static String Password;
+
+
+    /*private static String jdbcURL = "jdbc:mysql://localhost:3306/student";
     private static String jdbcUsername = "root";
-    private static String jdbcPassword = "root";
+    private static String jdbcPassword = "root";*/
     private static Connection con;
 
     public static Connection getConnection() {
-
+        System.out.println( Url + Username + Password );
         try {
             //Establishing a Connection
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            con = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
+            Class.forName( "com.mysql.cj.jdbc.Driver" );
+            con = DriverManager.getConnection( Url, Username, Password );
         } catch (SQLException e) {
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
@@ -24,4 +46,10 @@ public class StudentDBUtil {
         }
         return con;
     }
+
+
+
 }
+
+
+
